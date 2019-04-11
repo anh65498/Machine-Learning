7591a9045c5e0dcda511bee5dd74b0398f022d5d
Anh Pham <anh65498@gmail.com>
2019-04-11 16:49:08 -0700

    Finish all-vs-one logistic regression

--- 3_Neural_Network/Source Code/ex3.m
+++ 3_Neural_Network/Source Code/ex3.m
@@ -1,56 +1,30 @@
-%% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
-
-%  Instructions
-%  ------------
-%
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
+%% Logistic Regression: One-vs-all Classification
 %% Initialization
 clear ; close all; clc
 
-%% Setup the parameters you will use for this part of the exercise
+%% Setup the parameters
 input_layer_size  = 400;  % 20x20 Input Images of Digits
 num_labels = 10;          % 10 labels, from 1 to 10
                           % (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset.
-%  You will be working with a dataset that contains handwritten digits.
-%
 
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
-load('ex3data1.mat'); % training data stored in arrays X, y
-m = size(X, 1);
+load('ex3data1.mat');                   % 5000 training data stored in arrays X, y (automatically)
+m = size(X, 1);                         % 5000 training examples
 
 % Randomly select 100 data points to display
-rand_indices = randperm(m);
-sel = X(rand_indices(1:100), :);
+rand_indices = randperm(m);             % Return a row vector containing a random permutation of '1:m'.
+sel = X(rand_indices(1:100), :);        % take the first 100 numbers inside a vector of random numbers
 
-displayData(sel);
+displayData(sel);                       % display 100 random images of numbers
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
 %% ============ Part 2a: Vectorize Logistic Regression ============
-%  In this part of the exercise, you will reuse your logistic regression
-%  code from the last exercise. You task here is to make sure that your
-%  regularized logistic regression implementation is vectorized. After
-%  that, you will implement one-vs-all classification for the handwritten
-%  digit dataset.
-%
 
 % Test case for lrCostFunction
 fprintf('\nTesting lrCostFunction() with regularization');
@@ -85,4 +59,3 @@ pause;
 pred = predictOneVsAll(all_theta, X);
 
 fprintf('\nTraining Set Accuracy: %f\n', mean(double(pred == y)) * 100);
-
